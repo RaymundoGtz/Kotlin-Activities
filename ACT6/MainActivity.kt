d8372cf3c8acc6d35330d35f01be2a38ddf01b59
Raymundo Gutiérrez <129213631+RaymundoGtz@users.noreply.github.com>
2024-04-15 05:17:41 -0400

    Update MainActivity.kt

--- ACT6/MainActivity.kt
+++ ACT6/MainActivity.kt
@@ -1,61 +1,160 @@
-package com.iest.misdatos.viewmodels
+package com.iest.misdatos
 
 import android.content.Context
-import androidx.compose.ui.text.font.FontWeight
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.iest.misdatos.ui.theme.MisDatosTheme
+import com.iest.misdatos.viewmodels.PreferencesViweModel
+import kotlinx.coroutines.launch
+import java.time.format.TextStyle
 
-class PreferencesViweModel(val context: Context) {
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val context:Context = LocalContext.current
+            val preferencesViweModel = PreferencesViweModel(context)
+            MisDatosTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
 
-    companion object{
-        val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
-        val NAME = stringPreferencesKey( "name")
-        val AGE = intPreferencesKey("age")
-        val HOBBY = stringPreferencesKey("hobby")
-        val HEIGHT = intPreferencesKey("height")
-        val WEIGHT = intPreferencesKey("weight")
-    }
-    val age: Flow<Int> = context.dataStore.data.map {
-            preferences ->
-        // No type safety.
-        preferences[AGE] ?:0
-    }
+                    SettingsView(preferencesViweModel)
 
-    val name: Flow<String> = context.dataStore.data.map {
-        preferencies ->
-        preferencies[NAME] ?:""
-    }
-
-    val hobby: Flow<String> = context.dataStore.data.map {
-            preferencies ->
-        preferencies[HOBBY] ?:""
-    }
-
-    val height: Flow<Int> = context.dataStore.data.map {
-            preferencies ->
-        preferencies[HEIGHT] ?: 0
-    }
-
-    val weight: Flow<Int> = context.dataStore.data.map {
-            preferencies ->
-        preferencies[WEIGHT] ?: 0
-    }
-
-    //EQUIVALENTE A SETNAME Y SETAGE
-    suspend fun setNameAndAgeAndHobby(name:String, age:Int, hobby:String, height:Int, weight:Int){
-        context.dataStore.edit { settings ->
-            settings[NAME] = name
-            settings[AGE] = age
-            settings[HOBBY] = hobby
-            settings[HEIGHT] = height
-            settings[WEIGHT] = weight
+                }
+            }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun SettingsView(preferencesViewModel: PreferencesViweModel){
+    Column (Modifier.fillMaxSize()) {
+
+        var name by rememberSaveable{
+            mutableStateOf("0")
+        }
+
+        var age by rememberSaveable {
+            mutableStateOf("")
+
+        }
+
+        var hobby by rememberSaveable {
+            mutableStateOf("")
+
+        }
+
+        var height by rememberSaveable {
+            mutableStateOf("")
+
+        }
+
+        var weight by rememberSaveable {
+            mutableStateOf("")
+
+        }
+
+
+
+
+
+        var corrutinasScope = rememberCoroutineScope()
+        var savedAge = preferencesViewModel.age.collectAsState(initial = 0)
+        var savedName = preferencesViewModel.name.collectAsState(initial = "???")
+        var savedHobby = preferencesViewModel.hobby.collectAsState(initial = "???")
+        var savedHeight = preferencesViewModel.height.collectAsState(initial = 0)
+        var savedWeight= preferencesViewModel.weight.collectAsState(initial = 0)
+
+        val saved = if (savedName.value == "???") {
+            false
+        } else {
+            true
+        }
+
+
+
+        Column (Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+
+            if(saved){
+
+                Text(modifier = Modifier.padding(start = 25.dp, top = 20.dp, end = 25.dp), textAlign = TextAlign.Center, fontSize = 20.sp,text = "Tu nombre es ${savedName.value}, tienes ${savedAge.value}, tu hobby es ${savedHobby.value}, mides ${savedHeight.value} cm y pesas ${savedWeight.value}kg")
+
+
+            }else{
+
+                Text(modifier = Modifier
+                    .padding(bottom = 20.dp), fontSize = 20.sp, text = "Introduce tus datos")
+
+                TextField(label = {Text(text = "Nombre")}, value = name, onValueChange = {
+                    name = it
+                })
+
+                TextField(label = { Text(text = "Edad")}, value = age, onValueChange = {
+                    age = it
+                })
+
+                TextField(label = { Text(text = "Hobby")}, value = hobby, onValueChange = {
+                    hobby = it
+                })
+
+                TextField(label = { Text(text = "Altura")}, value = height, onValueChange = {
+                    height = it
+                })
+
+                TextField(label = { Text(text = "Peso")}, value = weight, onValueChange = {
+                    weight = it
+                })
+
+                Button(modifier = Modifier.padding(top = 20.dp), onClick = {
+                    corrutinasScope.launch {
+
+                        if(age.toIntOrNull() != null){
+                            preferencesViewModel.setNameAndAgeAndHobby(name, age.toInt(), hobby, height.toInt(), weight.toInt())
+                        }
+
+                    }
+                }) {
+                    Text(text = "Guardar Datos")
+
+                }
+
+            }
+
+        }
+
+
+
     }
 }
